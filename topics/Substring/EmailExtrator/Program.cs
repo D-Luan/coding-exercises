a0c2fd4e80b2bb2319410f2d170759e647a7c9c8
d-luan <dluan1285@gmail.com>
2026-08-04 17:20:19 -0300

    feat(topics): add exercises for extracting substrings from emails and links

---
+++ topics/Substring/EmailExtrator/Program.cs
@@ -0,0 +1,15 @@
+﻿string email = "beginner.dev@gmail.com";
+
+Console.WriteLine(EmailExtrator(email));
+
+static string EmailExtrator(string email)
+{
+    int index = email.IndexOf("@");
+
+    if (index == -1)
+    {
+        return "Invalid email";
+    }
+
+    return email.Substring(index + 1);
+}
---
+++ topics/Substring/LinkValidator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+string url = "https://gemini.google.com";
+bool result = LinkValidator(url);
+
+Console.WriteLine((result is true) ? "It's safe" : "Danger!!!");
+
+static bool LinkValidator(string url)
+{
+    bool start = url.StartsWith("https://", StringComparison.OrdinalIgnoreCase);
+    bool end = url.EndsWith(".com", StringComparison.OrdinalIgnoreCase);
+
+    return start && end;
+}
