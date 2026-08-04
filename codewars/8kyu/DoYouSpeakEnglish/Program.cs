32d2b577d13ae61bdc2fd9d273ce055faa33fe8b
d-luan <dluan1285@gmail.com>
2026-08-04 17:35:52 -0300

    feat(codewars): adds a solution to the 'Do you speak English?' problem

---
+++ codewars/8kyu/DoYouSpeakEnglish/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+string sentence = "abcEnglishdef";
+Console.WriteLine(SpeakEnglish(sentence));
+
+sentence = "abcnEglishsef";
+Console.WriteLine(SpeakEnglish(sentence));
+
+static bool SpeakEnglish(string sentence)
+{
+    return sentence.Contains("english", StringComparison.OrdinalIgnoreCase);
+}
