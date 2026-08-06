2feb152bb3ee8148a51a44ec78fd92f2357c9ff5
d-luan <dluan1285@gmail.com>
2026-08-06 15:51:50 -0300

    feat(topics): adds reversed-word and palindrome exercises

---
+++ topics/Strings/PalindromeDetector/Program.cs
@@ -0,0 +1,14 @@
+﻿string str = "arara";
+Console.WriteLine(IsPalindrome(str));
+
+str = "casa";
+Console.WriteLine(IsPalindrome(str));
+
+static bool IsPalindrome(string str)
+{
+    string strLower = str.ToLower();
+
+    string newStr = new String(strLower.Reverse().ToArray());
+
+    return newStr == str;
+}
---
+++ topics/Strings/SentenceReverser/Program.cs
@@ -0,0 +1,9 @@
+﻿string sentence = "Eu gosto de programar";
+sentence = ReverseWords(sentence);
+
+Console.WriteLine(sentence);
+
+static string ReverseWords(string sentence)
+{
+    return string.Join(" ", sentence.Split(' ').Reverse());
+}
