6ac99fb47bed333add9df86e475d0d467fc3ac3b
d-luan <dluan1285@gmail.com>
2026-08-06 15:11:08 -0300

    feat(codewars): adds the reversed string solution

---
+++ codewars/8kyu/ReversedStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿string str = "world";
+
+Console.WriteLine(ReverseString(str));
+
+static string ReverseString(string str)
+{
+    char[] array = str.ToCharArray();
+
+    Array.Reverse(array);
+
+    return new string(array);
+}
