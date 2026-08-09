e9cb1a5dfc45a325432d2f02ddf591b9441a40f0
d-luan <dluan1285@gmail.com>
2026-08-08 22:15:38 -0300

    feat(codewars): adds the solution to the exercise on removing exclamation marks

---
+++ codewars/8kyu/RemoveExclamation/Program.cs
@@ -0,0 +1,12 @@
+﻿string s = "Hi!!!";
+Console.WriteLine(RemoveExclamation(s));
+
+static string RemoveExclamation(string s)
+{
+    if (s.EndsWith("!"))
+    {
+        return s.Substring(0, s.Length - 1);
+    }
+
+    return s;
+}
