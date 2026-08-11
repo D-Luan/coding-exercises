10ba9279c7043446b8eb0b8f2cae0bdc77e7e7f2
d-luan <dluan1285@gmail.com>
2026-08-10 22:12:40 -0300

    feat(codewars): adds the exercise to convert a number to a string

---
+++ codewars/8kyu/ConvertNumberToString/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = 123;
+Console.WriteLine(NumberToString(num));
+
+num = 999;
+Console.WriteLine(NumberToString(num));
+
+num = -100;
+Console.WriteLine(NumberToString(num));
+
+static string NumberToString(int num)
+{
+    return num.ToString();
+}
