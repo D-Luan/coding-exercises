023ea0780a1419514faaa8536e7f4255cdb58172
d-luan <dluan1285@gmail.com>
2026-08-20 11:47:15 -0300

    feat(codewars): adds solutions to the exercises well of ideas and short long short

---
+++ codewars/8kyu/ShortLongShort/Program.cs
@@ -0,0 +1,9 @@
+﻿string a = "232";
+string b = "1";
+
+Console.WriteLine(ShortLongShort(a, b));
+
+static string ShortLongShort(string a, string b)
+{
+    return (a.Length < b.Length) ? (a + b + a) : (b + a + b);
+}
---
+++ codewars/8kyu/WellOfIdeas/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] x = { "bad", "bad", "bad" };
+Console.WriteLine(Well(x));
+
+static string Well(string[] x)
+{
+    int goodQty = x.Count(w => w == "good");
+
+    return goodQty switch
+    {
+        > 2 => "I smell a series!",
+        > 0 => "Publish!",
+        _ => "Fail!"
+    };
+}
