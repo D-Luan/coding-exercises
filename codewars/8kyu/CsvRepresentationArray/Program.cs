ee233eb4a6e781f76712be93e09f580ef7e3f4e3
d-luan <dluan1285@gmail.com>
2026-08-24 21:02:11 -0300

    feat(codewars): add solution for the exercise on representing an array as CSV

---
+++ codewars/8kyu/CsvRepresentationArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+int[][] array = 
+{
+    new[] { 0, 1, 2, 3, 45 },
+    new[] { 10, 11, 12, 13, 14 },
+    new[] { 20, 21, 22, 23, 24 },
+    new[] { 30, 31, 32, 33, 34 }
+};
+
+Console.WriteLine(ToCsvText(array));
+
+static string ToCsvText(int[][] array)
+{
+    return string.Join("\n", array.Select(row => string.Join(",", row)));
+}
