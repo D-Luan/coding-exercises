1da85e5ea9fe7d2739ec00511d23aaa6a0cefde8
d-luan <dluan1285@gmail.com>
2026-09-07 17:36:32 -0300

    feat(codewars): adds exercises to convert a string into an array and count sheep

---
+++ codewars/8kyu/CountSheep/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = 3;
+
+Console.WriteLine(CountSheep(n));
+
+static string CountSheep(int n)
+{
+    return string.Concat(Enumerable.Range(1, n).Select(i => $"{i} sheep..."));
+}
---
+++ codewars/8kyu/StringToArray/Program.cs
@@ -0,0 +1,9 @@
+﻿string str = "I love arrays they are my favorite";
+string[] array = StringToArray(str);
+
+foreach(string s in array)
+{
+    Console.WriteLine(s);
+}
+
+static string[] StringToArray(string str) => str.Split(" ");
