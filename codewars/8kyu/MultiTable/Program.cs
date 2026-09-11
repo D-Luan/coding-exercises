2f58639356d01d1bfa73f8ff464332805517bae5
d-luan <dluan1285@gmail.com>
2026-09-11 20:40:23 -0300

    feat(codewars): adds a solution to MultiTable exercise

---
+++ codewars/8kyu/MultiTable/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = 5;
+
+Console.WriteLine(MultiTable(number));
+
+static string MultiTable(int number)
+{
+    string result = "";
+
+    for (int i = 1; i <= 10; i++)
+    {
+        result = result + $"{i} * {number} = {i * number}";
+
+        if (i != 10) result += "\n";
+    }
+
+    return result;
+}
