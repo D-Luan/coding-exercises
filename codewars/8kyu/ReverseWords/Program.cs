4b92211f7669b4cf791d5c56a1fdfde7550ddfce
d-luan <dluan1285@gmail.com>
2026-09-17 17:43:45 -0300

    feat(codewars): adds solutions of the SumMix and ReverseWords exercises

---
+++ codewars/8kyu/ReverseWords/Program.cs
@@ -0,0 +1,8 @@
+﻿string str = "The greatest victory is that which requires no battle";
+
+Console.WriteLine(ReverseWords(str));
+
+static string ReverseWords(string str)
+{
+    return string.Join(" ", str.Split(' ').Reverse());
+}
---
+++ codewars/8kyu/SumMix/Program.cs
@@ -0,0 +1,5 @@
+﻿object[] x = { "3", 6, 6, 0, "5", 8, 5, "6", 2, "0" };
+
+Console.WriteLine(SumMix(x));
+
+static int SumMix(object[] x) => x.Sum(Convert.ToInt32);
