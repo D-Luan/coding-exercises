7601b00d63a910a6817b4379de41d883cf4a1ade
D-luan <dluan1285@gmail.com>
2026-09-22 20:42:08 -0300

    feat(codewars): adds solutions for the RemoveChar and IsUpperCase exercises

---
+++ codewars/8kyu/IsUpperCase/Program.cs
@@ -0,0 +1,5 @@
+﻿string text = "HELLO I AM DONALD";
+
+Console.WriteLine($"Text is Upper? {IsUpperCase(text)}");
+
+static bool IsUpperCase(string text) => text == text.ToUpper();
---
+++ codewars/8kyu/RemoveChar/Program.cs
@@ -0,0 +1,5 @@
+﻿string s = "eloquent";
+
+Console.WriteLine(RemoveChar(s));
+
+static string RemoveChar(string s) => s[1..^1];
