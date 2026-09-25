a2082d93bbc4d951eb533eb73624d06d747e5a27
D-luan <dluan1285@gmail.com>
2026-09-24 21:10:23 -0300

    feat(codewars): adds the bonustime solution

---
+++ codewars/8kyu/BonusTime/Program.cs
@@ -0,0 +1,9 @@
+﻿int salary = 10000;
+bool bonus = true;
+
+Console.WriteLine(BonusTime(salary, bonus));
+
+static string BonusTime(int salary, bool bonus)
+{
+    return bonus is true ? ("$" + salary * 10) : ("$" + salary);
+}
